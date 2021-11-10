a662f46e12dd9818ccaf87da4e89267551976783
tanimhasan <taniimhasan@gmail.com>
2021-11-11 00:09:25 +0600

    Initial commit

---
+++ app/src/main/java/com/tanim/ams/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.tanim.ams;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    int splashTimeOut=4000; //time in mile seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },splashTimeOut);
+    }
+}
